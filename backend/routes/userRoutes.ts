671d45cd0420807bc34f68ad00c2d49fa167da6e
MICHAELSWONG <mizik_jah@yahoo.com>
2020-10-14 16:55:42 -0400

    Custom Auth Middleware

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { authUser } from '../controllers/userController';
+import { authUser, getUserProfile } from '../controllers/userController';
+import { protect } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id: string) => {
-  return jwt.sign({ id }, String(process.env.JWT_TOKEN), {
+  return jwt.sign({ id }, String(process.env.JWT_SECRET), {
     expiresIn: '30d',
   });
 };
